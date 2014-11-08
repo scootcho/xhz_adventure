ac97665968127db104158bb443624269f0211ce6
Scott Yu <scott.tj.yu@gmail.com>
2014-11-08 23:20:27 +0800

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XhzAdventure::Application.config.secret_token = '034b267f124ddf067740b47139710ffd6e3f5a1be2db1f9bbbcd91d627c159698e6e29e424688c700898ce6e280b9874bb9723e25036843d3eb1acf43d5eb41d'
