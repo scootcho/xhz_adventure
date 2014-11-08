275c27a2e1625e37cbcc062fce1476434da2b342
Scott Yu <scott.tj.yu@gmail.com>
2014-11-08 23:52:55 +0800

    added menu tweak for 2.1.0

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def navigation_menu
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.css = "navbar-inner"
+    presenter.menu_tag = :div
+    presenter.list_tag_css = "nav"
+    presenter.selected_css = "active"
+    presenter.first_css = ""
+    presenter.last_css = ""
+    presenter.max_depth = 0 # prevents dropdown menus, which don't render correctly
+    presenter
+  end
+
 end
