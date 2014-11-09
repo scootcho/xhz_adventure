ed7073d3f5f299800b05f6c40cfca506e97fcf79
Scott Yu <scott.tj.yu@gmail.com>
2014-11-09 10:08:59 +0800

    switched to foundation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,21 @@ module ApplicationHelper
 
   def navigation_menu
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
-    presenter.css = 'navbar navbar-default" role="navigation'
+    presenter.css = "navbar-inner"
     presenter.menu_tag = :div
-    presenter.list_tag_css = "nav navbar-nav"
+    presenter.list_tag_css = "nav"
     presenter.selected_css = "active"
     presenter.first_css = ""
     presenter.last_css = ""
     presenter.max_depth = 0 # prevents dropdown menus, which don't render correctly
     presenter
   end
+
+# module ApplicationHelper
+#     def zurb_menu
+#         menu_items = Refinery::Menu.new(Refinery::Page.in_menu)
+#         presenter = Refinery::Pages::ZurbMenuPresenter.new(menu_items, self)
+#         presenter
+#     end
 
 end
