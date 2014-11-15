f7dcf9950e723931ac60f0052c09ef15af511150
Scott Yu <scott.tj.yu@gmail.com>
2014-11-15 09:06:32 +0800

    asset precompile true

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module XhzAdventure
     config.assets.enabled = true
 
     #Heroku Fix
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
