fe48c7306f99a1b75fe578b15335f665008b056a
Scott Yu <scott.tj.yu@gmail.com>
2014-12-22 20:38:58 +0800

    secret_token ready for production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-XhzAdventure::Application.config.secret_token = 'cd7ca4956cc9e5ab8c50b9139fcd5380de5ed52aed4077174b9a23028fdf541968713330bcb110797fc705091f1efcf3fa1096e37bb0365b9fe28b91ba5c5e31'
+
+XhzAdventure::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  'cd7ca4956cc9e5ab8c50b9139fcd5380de5ed52aed4077174b9a23028fdf541968713330bcb110797fc705091f1efcf3fa1096e37bb0365b9fe28b91ba5c5e31'
+else
+  ENV['SECRET_TOKEN']
+end
